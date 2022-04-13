0bbd514e6845934ec2a796a56bb6e6c057c5d3fa
Sean Hong(홍성민) <seanhong2000@gmail.com>
2022-04-14 02:16:59 +0900

    Fix typo

--- drone-wrapper/stats/views.py
+++ drone-wrapper/stats/views.py
@@ -1,36 +1,42 @@
 from rest_framework import viewsets
 import django_filters.rest_framework
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import ListCreateAPIView
-from .permissions import IsUserOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import ReadOnly
 from .models import Flight, Deck, Drone, FlightRecord
 from .serializers import FlightSerializer, DeckSerializer, DroneSerializer, FlightRecordSerializer
 
 
-class DroneViewSet(ListCreateAPIView, viewsets.GenericViewSet):
+class DroneViewSet(viewsets.ModelViewSet):
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
 
     def get_queryset(self):
-        user = self.request.user
-        return Drone.objects.filter(admin_id=user)
+        print("USER", self.request.user)
+        if self.request.user == 'AnonymousUser':
+            user = self.request.user
+            filtered_content = Drone.objects.filter(admin_id=user)
+        else:
+            filtered_content = Drone.objects.all()
+        return filtered_content
+
+
 
 
 class DeckViewSet(viewsets.ModelViewSet):
     queryset = Deck.objects.all()
     serializer_class = DeckSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class FlightRecordViewSet(viewsets.ModelViewSet):
     queryset = FlightRecord.objects.all()
     serializer_class = FlightRecordSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
